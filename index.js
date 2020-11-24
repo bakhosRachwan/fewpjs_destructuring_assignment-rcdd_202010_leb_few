56b7380cded93ad7af7d7e0953cb6affe8ad0266
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-24 21:30:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,3 +56,5 @@ const [indg] = colors.indigo;
 const {name, color, song, job, partner} = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song_2, song_4} =k_muppet.
+const {}
