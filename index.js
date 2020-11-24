d28f0a9042e4137d4fc66d547249f7e6a6e79ddc
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-24 21:33:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const [indg] = colors.indigo;
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
-const {name, color, song, job, partner} = muppet;
+const { name, color, song, job, partner} = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const {song_2, song_4} = k_muppet.k_album.the_muppet_movie;
